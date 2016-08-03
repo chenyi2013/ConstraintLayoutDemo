44f5f74c2b3e21675491819201e9550b958e7250
kevin chen <chenyi_de_email@163.com>
2016-08-03 11:24:53 +0800

    add conpon listview

--- app/src/main/java/ired/myapplication/MainActivity.java
+++ app/src/main/java/ired/myapplication/MainActivity.java
@@ -1,13 +1,77 @@
 package ired.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setDividerHeight(0);
+        listView.setAdapter(new ConponAdapter());
+    }
+
+
+    static class ViewHolder {
+        TopEdgeView topEdgeView;
+        TextView price;
+        TextView conpon;
+        TextView canUsePrice;
+        TextView status;
+        TextView time;
+        View bottomLine;
+
+
+        void setOverDay(boolean isOverDay) {
+            topEdgeView.setOverDay(isOverDay);
+            int color = isOverDay ? Color.parseColor("#cccccc") : Color.parseColor("#ff5b73");
+            price.setTextColor(color);
+            conpon.setTextColor(color);
+            status.setTextColor(color);
+            bottomLine.setBackgroundColor(color);
+        }
+    }
+
+
+    static class ConponAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+
+            if(view == null){
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+            }
+
+
+            return view;
+        }
     }
 }
